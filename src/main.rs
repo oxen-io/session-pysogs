71b2105a18a520ac5c6993a051b3412602b7fbf5
Niels Andriesse <andriesseniels@gmail.com>
2021-03-09 10:28:53 +1100

    Implement DB connection pooling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use rusqlite::{params, Connection};
+use rusqlite::{params};
+use r2d2_sqlite::SqliteConnectionManager;
 use serde::{Deserialize, Serialize};
 use warp::Filter;
 
@@ -10,8 +11,9 @@ struct Message {
 #[tokio::main]
 async fn main() {
     // Database
-    // TODO: Database file
-    let db_conn = Connection::open_in_memory().expect("Couldn't open database connection.");
+    let db_manager = SqliteConnectionManager::file("messages.db");
+    let db_pool = r2d2::Pool::new(db_manager).unwrap();
+    let db_conn = db_pool.get().unwrap();
 
     db_conn.execute(
         "CREATE TABLE messages (
@@ -26,25 +28,24 @@ async fn main() {
         .and(warp::path("messages"))
         .and(warp::body::content_length_limit(1024 * 256)) // Limit body size to 256 kb
         .and(warp::body::json()) // Expect JSON
-        .map(|message: Message| {
-            // TODO: Don't share database connection across threads
-            let db_conn = Connection::open_in_memory().expect("Couldn't open database connection.");
+        .map(move |message: Message| {
+            let db_conn = db_pool.get().unwrap(); // TODO: Fail gracefully
 
             db_conn.execute(
                 "INSERT INTO messages (text) VALUES (?1)",
                 params![message.text],
             ).expect("Couldn't insert message into database."); // TODO: Fail gracefully
 
-            // let mut stmt = db_conn.prepare("SELECT text FROM messages");
-            // let messages_iter = stmt.query_map([], |row| {
-            //     Ok(Message {
-            //         text: row.get(0)?
-            //     })
-            // })?;
+            let mut stmt = db_conn.prepare("SELECT text FROM messages").unwrap();
+            let mut messages_iter = stmt.query_map(params![], |row| {
+                Ok(Message {
+                    text: row.get(0).unwrap() // TODO: Fail gracefully
+                })
+            }).unwrap();
 
-            // for message in messages_iter {
-            //     println!("message {:?}", message.unwrap());
-            // }
+            for message in messages_iter {
+                println!("message {:?}", message.unwrap());
+            }
 
             warp::reply::json(&message)
         });
