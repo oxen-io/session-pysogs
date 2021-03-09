4a713729017e7e4d136d241871f3e5cc91f414c0
nielsandriesse <andriesseniels@gmail.com>
2021-03-10 08:51:40 +1100

    Separate database interaction

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 use rusqlite::{params};
-use r2d2_sqlite::SqliteConnectionManager;
 use warp::Filter;
 
 #[tokio::main]
 async fn main() {
     // Database
-    let db_manager = SqliteConnectionManager::file("messages.db");
-    let db_pool = r2d2::Pool::new(db_manager).unwrap();
-    let db_conn = db_pool.get().unwrap();
+    let db_pool = storage::get_db_pool();
+    let db_conn = storage::get_db_conn(&db_pool).unwrap(); // Force
 
     db_conn.execute(
         "CREATE TABLE IF NOT EXISTS messages (
@@ -26,17 +24,15 @@ async fn main() {
 }
 
 mod routes {
-    use r2d2_sqlite::SqliteConnectionManager;
     use warp::Filter;
 
     use super::handlers;
     use super::models::QueryOptions;
-
-    type DatabaseConnectionPool = r2d2::Pool<SqliteConnectionManager>;
+    use super::storage;
 
     /// POST /messages
     pub fn send_message(
-        db_pool: DatabaseConnectionPool,
+        db_pool: storage::DatabaseConnectionPool,
     ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
         warp::post()
             .and(warp::path("messages"))
@@ -50,7 +46,7 @@ mod routes {
     /// 
     /// Returns the last `count` messages.
     pub fn get_messages(
-        db_pool: DatabaseConnectionPool,
+        db_pool: storage::DatabaseConnectionPool,
     ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
         warp::get()
             .and(warp::path("messages"))
@@ -63,24 +59,17 @@ mod routes {
 mod handlers {
     use log;
     use rusqlite::params;
-    use r2d2_sqlite::SqliteConnectionManager;
     use warp::http::StatusCode;
 
     use super::models::Message;
     use super::models::QueryOptions;
-
-    type DatabaseConnection = r2d2::PooledConnection<SqliteConnectionManager>;
-    type DatabaseConnectionPool = r2d2::Pool<SqliteConnectionManager>;
-
-    #[derive(Debug)]
-    struct DatabaseError;
-    impl warp::reject::Reject for DatabaseError { }
+    use super::storage;
 
     /// Inserts the given `message` into the database.
-    pub async fn insert_message(message: Message, db_pool: DatabaseConnectionPool) -> Result<impl warp::Reply, warp::reject::Rejection> {
+    pub async fn insert_message(message: Message, db_pool: storage::DatabaseConnectionPool) -> Result<impl warp::Reply, warp::reject::Rejection> {
         // TODO: Validation
         // Get a database connection
-        let db_conn = get_db_conn(db_pool)?;
+        let db_conn = storage::get_db_conn(&db_pool)?;
         // Insert the message
         db_conn.execute(
             "INSERT INTO messages (text) VALUES (?1)",
@@ -91,12 +80,17 @@ mod handlers {
     }
 
     /// Returns the last `options.limit` messages from the database.
-    pub async fn get_messages(options: QueryOptions, db_pool: DatabaseConnectionPool) -> Result<impl warp::Reply, warp::reject::Rejection> {
+    pub async fn get_messages(options: QueryOptions, db_pool: storage::DatabaseConnectionPool) -> Result<impl warp::Reply, warp::reject::Rejection> {
         // Get a database connection
-        let db_conn = get_db_conn(db_pool)?;
+        let db_conn = storage::get_db_conn(&db_pool)?;
         // Query the database
-        let raw_query = "SELECT text FROM messages";
-        let mut query = get_db_query(&raw_query, db_conn)?;
+        let mut query = match db_conn.prepare("SELECT text FROM messages") {
+            Ok(query) => query,
+            Err(e) => { 
+                log::warn!("Couldn't create database query due to error: {:?}.", e);
+                return Err(warp::reject::custom(storage::DatabaseError)) 
+            }
+        };
         let messages: Result<Vec<Message>, rusqlite::Error> = query.query_map(params![], |row| {
             Ok(Message {
                 text: row.get(0).unwrap() // TODO: Fail gracefully
@@ -106,25 +100,7 @@ mod handlers {
         Ok(warp::reply::json(&messages.unwrap())) // TODO: Fail gracefully
     }
 
-    fn get_db_conn(db_pool: DatabaseConnectionPool) -> Result<DatabaseConnection, warp::reject::Rejection> {
-        match db_pool.get() {
-            Ok(db_conn) => return Ok(db_conn),
-            Err(e) => { 
-                log::warn!("Couldn't get database connection from pool due to error: {:?}.", e);
-                return Err(warp::reject::custom(DatabaseError)) 
-            }
-        }
-    }
-
-    fn get_db_query(raw_query: &str, db_conn: DatabaseConnection) -> Result<rusqlite::Statement, warp::reject::Rejection> {
-        match db_conn.prepare(raw_query) {
-            Ok(query) => return Ok(query),
-            Err(e) => { 
-                log::warn!("Couldn't create database query due to error: {:?}.", e);
-                return Err(warp::reject::custom(DatabaseError)) 
-            }
-        };
-    }
+    
 }
 
 mod models {
@@ -138,5 +114,32 @@ mod models {
     #[derive(Debug, Deserialize)]
     pub struct QueryOptions {
         pub limit: Option<u16>
+    }
+}
+
+mod storage {
+    use log;
+    use r2d2_sqlite::SqliteConnectionManager;
+
+    pub type DatabaseConnection = r2d2::PooledConnection<SqliteConnectionManager>;
+    pub type DatabaseConnectionPool = r2d2::Pool<SqliteConnectionManager>;
+
+    #[derive(Debug)]
+    pub struct DatabaseError;
+    impl warp::reject::Reject for DatabaseError { }
+
+    pub fn get_db_pool() -> DatabaseConnectionPool {
+        let db_manager = SqliteConnectionManager::file("messages.db");
+        return r2d2::Pool::new(db_manager).unwrap(); // Force
+    }
+
+    pub fn get_db_conn(db_pool: &DatabaseConnectionPool) -> Result<DatabaseConnection, warp::reject::Rejection> {
+        match db_pool.get() {
+            Ok(db_conn) => return Ok(db_conn),
+            Err(e) => { 
+                log::warn!("Couldn't get database connection from pool due to error: {:?}.", e);
+                return Err(warp::reject::custom(DatabaseError)) 
+            }
+        }
     }
 }
