4bb8922bccd978546bc4962d7e49e5fada4e3ee3
nielsandriesse <andriesseniels@gmail.com>
2021-03-10 11:33:19 +1100

    Handle errors

--- src/main.rs
+++ src/main.rs
@@ -75,18 +75,23 @@ mod handlers {
     use super::models;
     use super::storage;
 
-    /// Inserts the given `message` into the database.
+    /// Inserts the given `message` into the database if it's valid.
     pub async fn insert_message(message: models::Message, db_pool: storage::DatabaseConnectionPool) -> Result<impl warp::Reply, Rejection> {
+        // Validate the message
         if !message.is_valid() { return Err(warp::reject::custom(models::ValidationError)); }
         // Get a database connection
         let db_conn = storage::get_db_conn(&db_pool)?;
         // Insert the message
-        db_conn.execute(
+        match db_conn.execute(
             "INSERT INTO messages (text) VALUES (?1)",
             params![message.text],
-        ).expect("Couldn't insert message into database."); // TODO: Fail gracefully
-        // Return
-        return Ok(StatusCode::CREATED);
+        ) {
+            Ok(_) => return Ok(StatusCode::OK),
+            Err(e) => {
+                log::warn!("Couldn't insert message due to error: {:?}.", e);
+                return Err(warp::reject::custom(storage::DatabaseError)); 
+            }
+        }
     }
 
     /// Returns the last `options.limit` messages from the database.
@@ -94,20 +99,38 @@ mod handlers {
         // Get a database connection
         let db_conn = storage::get_db_conn(&db_pool)?;
         // Query the database
-        let mut query = match db_conn.prepare("SELECT text FROM messages") {
+        let limit = options.limit.unwrap_or(256); // Never return more than 256 messages at once
+        let raw_query = format!("SELECT text FROM messages ORDER BY rowid DESC LIMIT {}", limit); // Get the last `limit` messages
+        let mut query = match db_conn.prepare(&raw_query) {
             Ok(query) => query,
             Err(e) => { 
                 log::warn!("Couldn't create database query due to error: {:?}.", e);
-                return Err(warp::reject::custom(storage::DatabaseError)); 
+                return Err(warp::reject::custom(storage::DatabaseError));
             }
         };
-        let messages: Result<Vec<models::Message>, rusqlite::Error> = query.query_map(params![], |row| {
-            Ok(models::Message {
-                text: row.get(0).unwrap() // TODO: Fail gracefully
-            })
-        }).unwrap().into_iter().take(options.limit.unwrap_or(std::u16::MAX).into()).collect(); // TODO: Fail gracefully
+        let rows = match query.query_map(params![], |row| {
+            Ok(models::Message { text: row.get(0)? })
+        }) {
+            Ok(rows) => rows,
+            Err(e) => {
+                log::warn!("Couldn't query database due to error: {:?}.", e);
+                return Err(warp::reject::custom(storage::DatabaseError));
+            }
+        };
+        // FIXME: It'd be cleaner to do the below using `collect()`, but the compiler has trouble
+        // inferring the item type of `rows` in that case.
+        let mut messages: Vec<models::Message> = Vec::new();
+        for row in rows {
+            match row {
+                Ok(message) => messages.push(message),
+                Err(e) => {
+                    log::warn!("Excluding message from response due to database error: {:?}.", e);
+                    continue;
+                }
+            }
+        }
         // Return the messages
-        return Ok(warp::reply::json(&messages.unwrap())); // TODO: Fail gracefully
+        return Ok(warp::reply::json(&messages));
     }
 }
 
