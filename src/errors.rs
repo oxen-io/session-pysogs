2bfd2de6fb487c32fc39a38959bce47007a73518
Niels Andriesse <andriesseniels@gmail.com>
2021-03-12 16:56:40 +1100

    Document errors

--- src/errors.rs
+++ src/errors.rs
@@ -3,9 +3,13 @@
 pub enum Error {
     DecryptionFailed,
     DatabaseFailedInternally,
+    /// No valid request could be formed. Usually this means the endpoint or 
+    /// HTTP method specified in the RPC call was malformed.
     InvalidRequest,
+    /// Couldn't parse LSRPC request to a valid RPC call.
     ParsingFailed,
     Unauthorized,
+    /// The provided data is invalid.
     ValidationFailed
 }
 impl warp::reject::Reject for Error { }
