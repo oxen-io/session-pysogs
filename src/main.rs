717734b7ae67b359423c85aca8e1de55bf3b51cd
Jeff Becker <jeff@i2p.rocks>
2021-03-21 09:04:00 -0400

    make all options configuable on runtime
    * pull in argparse to parse cli arguments
    * make ip and port configurable on runtime
    * add plaintext mode for use behind reverse proxies
    * make tls cert/key path configuable on runtime

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,45 @@ mod onion_requests;
 mod routes;
 mod rpc;
 mod storage;
+use argparse::{ArgumentParser, StoreTrue, Store};
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+
 #[cfg(test)]
 mod tests;
 
+
 #[tokio::main]
 async fn main() {
+
+    // arguments default values
+    let mut plaintext = false;
+    let mut tls_certfile = "tls_certificate.pem".to_string();
+    let mut tls_keyfile = "tls_private_key.pem".to_string();
+    let mut port : u16 = 443;
+    let mut bind_ip : Ipv4Addr = Ipv4Addr::new(0,0,0,0);
+    // parse arguments
+    {
+        let mut ap = ArgumentParser::new();
+        ap.set_description("session open group server");
+        ap.refer(&mut plaintext)
+            .add_option(&["--plaintext"], StoreTrue,
+                        "run in plaintext mode for use behind a reverse proxy");
+        ap.refer(&mut tls_certfile)
+            .add_option(&["--tls-cert"], Store,
+                        "path to tls certificate");
+        ap.refer(&mut tls_keyfile)
+            .add_option(&["--tls-key"], Store,
+                        "path to tls private key");
+        ap.refer(&mut port)
+            .add_option(&["-P", "--port"], Store,
+                        "Set the port to bind on");
+        ap.refer(&mut bind_ip)
+            .add_option(&["-H", "--host"], Store,
+                        "set ip to bind on");
+        ap.parse_args_or_exit();
+    }
+    // create socket address
+    let addr = SocketAddr::new(IpAddr::V4(bind_ip), port);
     // Print the server public key
     let public_key = hex::encode(crypto::PUBLIC_KEY.as_bytes());
     println!("The public key of this server is: {}", public_key);
@@ -27,18 +61,23 @@ async fn main() {
     fs::create_dir_all("./files").unwrap();
     // Create the main room
     let main_room = "main";
-    storage::create_database_if_needed(main_room);    
+    storage::create_database_if_needed(main_room);
     // Set up pruning jobs
     let prune_pending_tokens_future = storage::prune_pending_tokens_periodically();
     let prune_tokens_future = storage::prune_tokens_periodically();
     let prune_files_future = storage::prune_files_periodically();
     // Serve routes
-    let routes = routes::root().or(routes::lsrpc());
-    let serve_routes_future = warp::serve(routes)
-        .tls()
-        .cert_path("tls_certificate.pem")
-        .key_path("tls_private_key.pem")
-        .run(([0, 0, 0, 0], 443));
-    // Keep futures alive
-    join!(prune_pending_tokens_future, prune_tokens_future, prune_files_future, serve_routes_future);
+    let routes = warp::serve(routes::root().or(routes::lsrpc()));
+    if plaintext {
+        println!("!!! running in plaintext mode on {}", addr);
+        let serve_routes_future = routes.run(addr);
+        join!(prune_pending_tokens_future, prune_tokens_future, prune_files_future, serve_routes_future);
+    } else {
+        println!("running on {} with tls", addr);
+        let serve_routes_future = routes.tls()
+            .cert_path(tls_certfile)
+            .key_path(tls_keyfile)
+            .run(addr);
+        join!(prune_pending_tokens_future, prune_tokens_future, prune_files_future, serve_routes_future);
+    }
 }
