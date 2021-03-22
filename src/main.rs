b78d89a6066f263d630b64adb7f3dd16072a1d81
Niels Andriesse <andriesseniels@gmail.com>
2021-03-22 15:45:58 +1100

    Minor refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::fs;
 
+use argparse::{ArgumentParser, StoreTrue, Store};
 use futures::join;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use tokio;
 use warp::Filter;
 
@@ -12,45 +14,34 @@ mod onion_requests;
 mod routes;
 mod rpc;
 mod storage;
-use argparse::{ArgumentParser, StoreTrue, Store};
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
 #[cfg(test)]
 mod tests;
 
-
 #[tokio::main]
 async fn main() {
-
-    // arguments default values
     let mut plaintext = false;
-    let mut tls_certfile = "tls_certificate.pem".to_string();
-    let mut tls_keyfile = "tls_private_key.pem".to_string();
-    let mut port : u16 = 443;
-    let mut bind_ip : Ipv4Addr = Ipv4Addr::new(0,0,0,0);
-    // parse arguments
+    let mut tls_cert_file = "tls_certificate.pem".to_string();
+    let mut tls_priv_key_file = "tls_private_key.pem".to_string();
+    let mut port: u16 = 443;
+    let mut ip = Ipv4Addr::new(0, 0, 0, 0);
+    // Parse command line arguments
     {
         let mut ap = ArgumentParser::new();
         ap.set_description("session open group server");
         ap.refer(&mut plaintext)
-            .add_option(&["--plaintext"], StoreTrue,
-                        "run in plaintext mode for use behind a reverse proxy");
-        ap.refer(&mut tls_certfile)
-            .add_option(&["--tls-cert"], Store,
-                        "path to tls certificate");
-        ap.refer(&mut tls_keyfile)
-            .add_option(&["--tls-key"], Store,
-                        "path to tls private key");
+            .add_option(&["--plaintext"], StoreTrue, "run in plaintext mode for use behind a reverse proxy");
+        ap.refer(&mut tls_cert_file)
+            .add_option(&["--tls-cert"], Store, "path to tls certificate");
+        ap.refer(&mut tls_priv_key_file)
+            .add_option(&["--tls-key"], Store, "path to tls private key");
         ap.refer(&mut port)
-            .add_option(&["-P", "--port"], Store,
-                        "Set the port to bind on");
-        ap.refer(&mut bind_ip)
-            .add_option(&["-H", "--host"], Store,
-                        "set ip to bind on");
+            .add_option(&["-P", "--port"], Store, "set port to bind to");
+        ap.refer(&mut ip)
+            .add_option(&["-H", "--host"], Store, "set ip to bind to");
         ap.parse_args_or_exit();
     }
-    // create socket address
-    let addr = SocketAddr::new(IpAddr::V4(bind_ip), port);
+    let addr = SocketAddr::new(IpAddr::V4(ip), port);
     // Print the server public key
     let public_key = hex::encode(crypto::PUBLIC_KEY.as_bytes());
     println!("The public key of this server is: {}", public_key);
@@ -67,17 +58,20 @@ async fn main() {
     let prune_tokens_future = storage::prune_tokens_periodically();
     let prune_files_future = storage::prune_files_periodically();
     // Serve routes
-    let routes = warp::serve(routes::root().or(routes::lsrpc()));
+    let routes = routes::root().or(routes::lsrpc());
     if plaintext {
-        println!("!!! running in plaintext mode on {}", addr);
-        let serve_routes_future = routes.run(addr);
+        println!("Running in plaintext mode on {}.", addr);
+        let serve_routes_future = warp::serve(routes).run(addr);
+        // Keep futures alive
         join!(prune_pending_tokens_future, prune_tokens_future, prune_files_future, serve_routes_future);
     } else {
-        println!("running on {} with tls", addr);
-        let serve_routes_future = routes.tls()
-            .cert_path(tls_certfile)
-            .key_path(tls_keyfile)
+        println!("Running on {} with TLS.", addr);
+        let serve_routes_future = warp::serve(routes)
+            .tls()
+            .cert_path(tls_cert_file)
+            .key_path(tls_priv_key_file)
             .run(addr);
+        // Keep futures alive
         join!(prune_pending_tokens_future, prune_tokens_future, prune_files_future, serve_routes_future);
     }
 }
