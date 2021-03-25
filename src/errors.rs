661717d26473d48349e56ed8a0ca6161e488b93c
Niels Andriesse <andriesseniels@gmail.com>
2021-03-25 11:05:47 +1100

    Make switch work with auto-formatting

--- src/errors.rs
+++ src/errors.rs
@@ -12,13 +12,11 @@ pub enum Error {
 }
 impl warp::reject::Reject for Error {}
 
+#[rustfmt::skip]
 pub fn into_response(e: Rejection) -> Result<Response, Rejection> {
     if let Some(error) = e.find::<Error>() {
         match error {
-            Error::DecryptionFailed
-            | Error::InvalidOnionRequest
-            | Error::InvalidRpcCall
-            | Error::ValidationFailed => return Ok(StatusCode::BAD_REQUEST.into_response()),
+            Error::DecryptionFailed | Error::InvalidOnionRequest | Error::InvalidRpcCall | Error::ValidationFailed => return Ok(StatusCode::BAD_REQUEST.into_response()),
             Error::Unauthorized => return Ok(StatusCode::FORBIDDEN.into_response()),
             Error::DatabaseFailedInternally => {
                 return Ok(StatusCode::INTERNAL_SERVER_ERROR.into_response())
