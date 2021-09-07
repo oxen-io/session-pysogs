3d99ba221a0c6e820266fad479e48fc07ad14877
Jason Rhinelander <jason@imaginary.ca>
2021-09-07 14:38:32 -0300

    Rust version compatibility
    Because it should still work even if your rustc is more than 5 minutes
    old.

--- src/handlers.rs
+++ src/handlers.rs
@@ -136,7 +136,7 @@ pub async fn store_file(
     pool: &storage::DatabaseConnectionPool,
 ) -> Result<Response, Rejection> {
     // It'd be nice to use the UUID crate for the file ID, but clients want an integer ID
-    const UPPER_BOUND: u64 = 2u64.pow(53); // JS has trouble if we go higher than this
+    const UPPER_BOUND: u64 = 1u64 << 53; // JS has trouble if we go higher than this
     let id: u64 = thread_rng().gen_range(0..UPPER_BOUND);
     let now = chrono::Utc::now().timestamp();
     // Check authorization level if needed
