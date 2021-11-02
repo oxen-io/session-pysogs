351059ddc811ff0937fdec326c05ccfe2ec1511d
Jason Rhinelander <jason@imaginary.ca>
2021-11-02 11:46:10 -0300

    Monkey patch @app.get, etc. into old Flask

--- sogs/web.py
+++ sogs/web.py
@@ -1,3 +1,14 @@
 import flask
 
 app = flask.Flask(__name__)
+
+# Monkey-patch app.get/post/etc. for Flask <2 compatibility
+if not hasattr(flask.Flask, 'post'):
+    def _add_flask_method(name):
+        def meth(self, rule: str, **options):
+            return self.route(rule, methods=[name.upper()], **options)
+
+        setattr(flask.Flask, name, meth)
+
+    for method in ('get', 'post', 'put', 'delete', 'patch'):
+        _add_flask_method(method)
