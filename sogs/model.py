8af31f6b3c70acc414ffff9bb42f6ca2c4ae10e0
Jason Rhinelander <jason@imaginary.ca>
2021-12-21 14:26:12 -0400

    Fix profanity custom file loading

--- sogs/model.py
+++ sogs/model.py
@@ -20,7 +20,7 @@ if config.PROFANITY_FILTER:
     import better_profanity
 
     if config.PROFANITY_CUSTOM:
-        better_profanity.profanity.load_censor_words_from_file(config.PROFANITY_FILTER)
+        better_profanity.profanity.load_censor_words_from_file(config.PROFANITY_CUSTOM)
     else:
         better_profanity.profanity.load_censor_words()
 
