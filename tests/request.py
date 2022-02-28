e2a01284d0da4c5d6d33be4590f1f5528c526bcd
Jeff <jeff@oxen.io>
2022-02-28 13:03:07 -0500

    add endpoints for deleting posts from a room.
    * allow users to delete their own posts
    * allow mods to delete posts as needed
    * add unit tests for all above

--- tests/request.py
+++ tests/request.py
@@ -10,6 +10,14 @@ def sogs_get(client, url, user):
     return client.get(url, headers=x_sogs_for(user, "GET", url))
 
 
+def sogs_delete(client, url, user):
+    """
+    DETELEs a test `client` request to `url` with X-SOGS-* signature headers signing the request for
+    `user`.
+    """
+    return client.delete(url, headers=x_sogs_for(user, "DELETE", url))
+
+
 def sogs_post(client, url, json, user):
     """
     POSTs a test `client` request to `url` with the given `json` as body and X-SOGS-* signature
