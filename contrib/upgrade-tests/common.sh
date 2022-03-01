8b45be87f0e03b91bf5f65e107aae264e174357f
Jason Rhinelander <jason@imaginary.ca>
2022-03-01 13:32:56 -0400

    Make sure no more migrations needed after migrating
    Runs the --check-upgrades at the end of the migration to make sure the
    migration code doesn't still think a migration is needed.
    
    Also dry out the 0.1.10 upgrade script to use the logic from common.sh.

--- contrib/upgrade-tests/common.sh
+++ contrib/upgrade-tests/common.sh
@@ -43,24 +43,63 @@ done
 
 
 do_upgrades() {
+    first=1
     tags=("$@" "$(git rev-parse HEAD)")
     for tag in "${tags[@]}"; do
         echo "Upgrading to $tag..."
         git -c advice.detachedHead=false checkout "$tag"
 
-        args=("-L")
+        # Use the dedicated --upgrade command if it has been added in this revision:
+        if [ -e sogs/__main__.py ] && grep -q '^ *"--upgrade",$' sogs/__main__.py; then
+            args=("--upgrade")
+        else
+            # Before it was added, any command would implicitly upgrade:
+            args=("-L")
+        fi
 
         if [ -n "$first" ]; then
             first=
+
+            if [ -n "$sogs_key_conv" ]; then
+                python3 -msogs.key_convert
+
+                echo "Checking key_x25519 for proper conversion"
+                diff --color=always \
+                    <(python3 -c 'f = open("key_x25519", "rb"); print(f.read().hex())') \
+                    <(echo a0101f8bca7fa1cedf9620f5b80810b18f5b0f1acbb219640876be9d78a6195f)
+            fi
 
             # In 0.2.0 and up until close to 0.3.0, just running any command-line commands will do the
             # database import and/or upgrade.  Starting in 0.3.0 you have to specify --initialize to make
             # this happen.
             if [ -e sogs/__main__.py ] && grep -q '^ *"--initialize",$' sogs/__main__.py; then
-                args+=("--initialize")
+                if [ "${args[0]}" == "--upgrade" ]; then
+                    # If we support the --upgrade flag then --initialize and --upgrade are exclusive:
+                    args=("--initialize")
+                else
+                    args+=("--initialize")
+                fi
             fi
         fi
 
         python3 -msogs "${args[@]}"
+
+        if [ -n "$sogs_fix_updates_count" ]; then
+            # 0.2.0 had a bug in one of the room update triggers that would unnecessarily update
+            # `message_sequence` (then named `updates`) on metadata updates, which the 0.1.x import
+            # triggered when setting the image value.  This was fixed before v0.3.0, but if our
+            # first tag imports via such a problematic version then we need to undo the increment so
+            # that the final message_sequence value remains comparable to a version that imported
+            # directly into a newer release.
+            if sed -ne '/^CREATE TRIGGER room_metadata_update/,/^END;/p' sogs/schema.sql* \
+                | grep -q 'SET updates = updates + 1'; then
+                sqlite3 sogs.db 'UPDATE rooms SET updates = updates - 1 WHERE image IS NOT NULL'
+            fi
+            sogs_fix_updates_count=
+        fi
     done
+
+    # This should exit cleanly to indicate no needed migrations (if it doesn't, i.e. we still
+    # require migrations after doing a migration then Something Getting Wrong in migrations).
+    python3 -msogs --check-upgrades
 }
