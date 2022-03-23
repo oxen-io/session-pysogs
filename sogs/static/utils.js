ba5e7a7e443930b1b5b94d649445fa0dae8eb1ae
koray-eren <koray.eren@outlook.com>
2022-03-23 05:37:12 +0000

    minor refactor

--- sogs/static/utils.js
+++
@@ -1,8 +0,0 @@
-function toggleDarkMode() {
-    var element = document.getElementById("html");
-    if (element.classList.contains('dark') ) {
-        element.classList.remove('dark')
-    } else {
-        element.classList.add('dark');
-    }
-}
