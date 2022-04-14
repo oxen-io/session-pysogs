41be1305e845e1078248238137c36744db7f86a5
koray-eren <koray.eren@outlook.com>
2022-04-14 03:36:48 +0000

    cleanup comments and debugs prints

--- sogs/routes/views.py
+++ sogs/routes/views.py
@@ -34,40 +34,6 @@ def serve_index():
 
 @views.get("/r/<Room:room>/")
 def view_room(room):
-    messageId = 100
-    response = ''
-    print()
-    try:
-        # response = message_single(room=room, msg_id=messageId)
-        response = messages_recent(room=room)
-        print('   +++   NO ERROR   +++   ')
-        if response.status_code == 200:
-            print("     ===     EXISTS      ===   ")
-                        
-            request_data = response.json[0]['data']
-            signature = response.json[0]['signature']
-            request = response.json[0]
-            epoch_time = request['posted']
-            decoded_data = decode_base64(request_data)
-            print('protobuf')
-            print(request_data)
-            # content = protobuf.Content()
-            print(protobuf.Content().ParseFromString(decoded_data) )
-            print()
-            print('MSG BODY:')
-            print(type(response.json[0]) )
-            print(request )
-            print(message_contents(decoded_data) )
-            my_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(epoch_time))
-            print(my_time)
-            # print(msg )
-            print()
-        else:
-            print("   !!!   NO MESSAGE  !!!   ")
-        print()
-    except Exception as e:
-        print("ERROR MSG: {}".format(e))
-    
     if not room.default_read:
         abort(http.FORBIDDEN)
 
