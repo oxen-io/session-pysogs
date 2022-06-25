b81b8418ad4cde2819c6d2ae5e4f0551537028fc
Jason Rhinelander <jason@imaginary.ca>
2022-06-24 21:22:43 -0300

    Document public rooms

--- sogs/routes/views.py
+++ sogs/routes/views.py
@@ -17,6 +17,11 @@ views = Blueprint('views', __name__)
 
 @views.get("/")
 def serve_index():
+    """
+    Publicly accessible URL that displays a list of public rooms to a web browser.  This isn't a
+    normal SOGS client endpoint, but rather a convenience web page for people who follow a SOGS
+    pseudo-URL.
+    """
     rooms = get_accessible_rooms()
     if len(rooms) == 0:
         return render_template('setup.html')
@@ -29,6 +34,11 @@ def serve_index():
 
 @views.get("/r/<Room:room>/")
 def view_room(room):
+    """
+    Publicly accessible URL that displays a room (including recent messages) to a web browser.  This
+    isn't a normal SOGS client endpoint, but rather a convenience web page for people who follow a
+    SOGS pseudo-URL, that displays the SOGS URL and QR code along with a list of recent messages.
+    """
     if not room.default_read:
         abort(http.FORBIDDEN)
 
@@ -41,6 +51,12 @@ def view_room(room):
 
 @views.get("/r/<Room:room>/invite.png")
 def serve_invite_qr(room):
+    """
+    URL that generates a SOGS open group URL in QR code format for consumption by a mobile device.
+    This isn't a normal SOGS client endpoint, but rather part of the a convenience web page for
+    people who view the SOGS URL in a browser and want to scan the URL into another device (i.e.
+    mobile Session).
+    """
     if not room.default_read:
         abort(http.FORBIDDEN)
 
